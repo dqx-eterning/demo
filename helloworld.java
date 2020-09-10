d9d0afc1067d1ac29dc772dfd8b239441bbd88a2
dqx_eterning <1986813958@qq.com>
2020-09-10 12:06:10 +0800

    helloworld

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public Class Test{
+	public static void main(String[] args){
+		System.out.println("hello world");
+		System.out.println("1");
+		System.out.println("hello world");
+		System.out.println("2");
+	}
+}
